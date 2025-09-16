048f398e29427f5e58b69de8d916ab9385320a6a
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-16 22:32:50 +0200

    Another one

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,15 +1,10 @@
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
