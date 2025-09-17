b3dc024c04bf214d668e71cf35911069c14671ba
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-17 08:30:42 +0200

    click

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  setTimeout(() => {
-    setCounter(counter + 1)
-  }, 1000)
-
-  console.log('rendering...', counter)
+  const [ counter, setCounter ] = useState(0)
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={() => console.log('clicked')}>
+        plus
+      </button>
+    </div>
   )
 }
-
 export default App
