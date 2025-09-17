0a31f662c21983d1253523023a2c3a1b4910fa60
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-17 12:24:32 +0200

    test right left

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <div>{counter}</div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
 
-      <button onClick={() => console.log('clicked')}>
-        plus
-      </button>
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
