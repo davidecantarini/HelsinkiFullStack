f0e778bb58f70e930388cecae3b6b393ab2e43a8
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-17 13:07:42 +0200

    Final

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -8,6 +8,7 @@ const History = (props) => {
       </div>
     )
   }
+
   return (
     <div>
       button press history: {props.allClicks.join(' ')}
@@ -15,33 +16,30 @@ const History = (props) => {
   )
 }
 
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  const [total, setTotal] = useState(0)
-
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
-  };
-  
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
       {right}
       <History allClicks={allClicks} />
     </div>
