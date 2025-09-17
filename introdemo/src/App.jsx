ff2deeab1716f850669cabcaee8b8f39b652374c
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-17 12:50:58 +0200

    set Total

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
 
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     setLeft(left + 1)
-  }
 
+    setTotal(left + right)
+  }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+
+    setTotal(left + right)
   }
 
   return (
@@ -23,9 +27,11 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }
+
 export default App
