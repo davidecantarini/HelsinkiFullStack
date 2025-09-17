7e5e97bb0565992099e52f495fdef440ff6446c1
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-17 08:27:08 +0200

    import userstate

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from 'react'
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
+  setTimeout(() => {
+    setCounter(counter + 1)
+  }, 1000)
 
   console.log('rendering...', counter)
 
@@ -13,4 +13,5 @@ const App = () => {
     <div>{counter}</div>
   )
 }
+
 export default App
