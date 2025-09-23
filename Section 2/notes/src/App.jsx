febebf0924681ed6afb5de68eeb650d158904ff2
Davide Cantarini <dav.cantarini@gmail.com>
2025-09-23 16:08:41 +0200

    Fixing

--- Section 2/notes/src/App.jsx
+++ Section 2/notes/src/App.jsx
@@ -1,45 +1,49 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
-
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  // Fetch all notes from backend on component mount
   useEffect(() => {
-  noteService.getAll().then(initialNotes => {
-    setNotes(initialNotes)
-  })
-}, [])
+    noteService.getAll().then(initialNotes => {
+      setNotes(initialNotes)
+    })
+  }, [])
 
-noteService.create(noteObject).then(returnedNote => {
-  setNotes(notes.concat(returnedNote))
-  setNewNote('')
-})
+  // Add a new note
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
 
-noteService.update(id, changedNote).then(returnedNote => {
-  setNotes(notes.map(note => note.id === id ? returnedNote : note))
-})
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
+    noteService.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
   }
 
   // Toggle importance of a note
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find((n) => n.id === id)
+    const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((note) => (note.id === id ? response.data : note)))
+    noteService.update(id, changedNote).then(returnedNote => {
+      setNotes(notes.map(n => n.id === id ? returnedNote : n))
     })
   }
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+  // Decide which notes to show
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -50,7 +54,7 @@ noteService.update(id, changedNote).then(returnedNote => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => (
+        {notesToShow.map(note => (
           <Note
             key={note.id}
             note={note}
